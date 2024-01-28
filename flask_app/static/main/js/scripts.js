8d9ba3b8f68d9b4fe3757f1756a76a61c25a58dc
OkayJerry <geraldjhoskinsiii@gmail.com>
2024-01-27 22:18:21 -0500

    temp

--- flask_app/static/main/js/scripts.js
+++ flask_app/static/main/js/scripts.js
@@ -13,7 +13,31 @@ function addCardToToolbar() {
 }
 
 function copyCardToNav(card) {
-    var nav = document.querySelector('nav');
-    var copiedCard = card.cloneNode(true); // Clone the card, including its children and attributes
-    nav.appendChild(copiedCard);
+    // Create a new `card-column`
+    var newColumn = document.createElement("div");
+    newColumn.className = "card-column";
+    
+    // Clone the card, including its children and attributes
+    var cardCopy = card.cloneNode(true);
+
+    // Remove `on click` response
+    cardCopy.onclick = null;
+
+    // Add the `card` to the `card-column`
+    newColumn.appendChild(cardCopy)
+
+    // Add the `card-column` to the `nav`
+    document.querySelector('nav').appendChild(newColumn);
+
+    addDutyToColumn(newColumn);
+}
+
+function addDutyToColumn(cardColumn) {
+    // Create a new `duty`
+    var duty = document.createElement("div");
+    duty.className = "duty";
+
+    var yPos = cardColumn.childElementCount * 50;
+    duty.style.top = yPos + 'px';
+    cardColumn.appendChild(childDiv);
 }
